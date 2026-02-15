ac92e4e1b15377fb79cb4d320c19c10362fd1ede
paintgree <paintgree@gmail.com>
2026-02-15 18:55:44 +0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -182,9 +182,9 @@ import openpyxl
 from fastapi import HTTPException
 from fastapi.responses import Response
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).resolve().parent
+TEMPLATE_DIR = BASE_DIR / "templates" / "templates_xlsx"
 
-MRR_TEMPLATE_DIR = os.path.join(BASE_DIR, "templates" / "templates_xlsx")
 
 MRR_TEMPLATE_XLSX_MAP = {
     # Your official RAW material report template
@@ -4730,6 +4730,7 @@ def mrr_photo_delete(
     session.commit()
 
     return RedirectResponse(f"/mrr/{lot_id}/inspection/id/{inspection_id}", status_code=303)
+
 
 
 
