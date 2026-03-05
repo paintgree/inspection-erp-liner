d59d561a4a39f15b3d1ca15310aff824a0bbe951
paintgree <paintgree@gmail.com>
2026-03-05 14:35:17 +0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,7 +7,6 @@ from sqlmodel import SQLModel, Field
 
 from datetime import datetime
 from typing import Optional
-from sqlmodel import SQLModel, Field
 
 class RunApproval(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
