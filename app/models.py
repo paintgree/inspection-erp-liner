77d987c2321c02256a12f403fe58508088c65c2b
paintgree <paintgree@gmail.com>
2026-04-08 14:51:29 +0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -898,6 +898,7 @@ class ManagedDocument(SQLModel, table=True):
     title: str = Field(default="", index=True)
     subtitle: str = Field(default="")
     category: str = Field(default="", index=True)
+    classification: str = Field(default="", index=True)
     description: str = Field(default="")
     revision: str = Field(default="")
 
