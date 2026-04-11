1684921779eb437011fbb08fc4c64c223e09a1e4
paintgree <paintgree@gmail.com>
2026-04-11 09:40:34 +0400

    Update db.py

--- app/db.py
+++ app/db.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import os
 from sqlmodel import SQLModel, Session, create_engine
-
+from sqlalchemy import inspect, text
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
